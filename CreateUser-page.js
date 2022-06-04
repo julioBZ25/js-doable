8d7004fb632a9c0747cf485b913af178ac96b942
Julio Bazalar <jbzuloaga@gmail.com>
2022-06-03 20:07:29 -0500

    ADD: create user logic

--- CreateUser-page.js
+++ CreateUser-page.js
@@ -1,23 +1,21 @@
 import { input } from "./input.js";
-import { login } from "./Login-service.js"
+import { signup } from "./Login-service.js"
 import DOMHandler from "./DOM.js";
 import HomePage from "./Home-page.js"
-import STORE from "./store.js";
-import signUpPage from "./CreateUser-page.js";
+import LoginPage from "./Login-page.js";
 
 function render() {
-  const { loginError } = LoginPage.state;
+  const { signUpError } = signUpPage.state;
   return `
       <section class="container">
-        <h1 class="heading heading--lg text-center mb-4">Login</h1>
-        <form class="flex flex-column gap-4 mb-4 js-login-form">
+        <h1 class="heading heading--lg text-center mb-4">Sign Up</h1>
+        <form class="flex flex-column gap-4 mb-4 js-signup-form">
           ${input({
             label: "email",
             id: "email",
             type: "email",
             placeholder: "tumail@mail.com",
             required: true,
-            value: "testdoable@mail.com"
           })}
           ${input({
             label: "password",
@@ -25,64 +23,58 @@ function render() {
             type: "password",
             placeholder: "******",
             required: true,
-            value: "123456"
           })}
-          ${loginError ? 
-            `<p class="text-center error-300">${loginError}</p>`: ''
+          ${signUpError ? 
+            `<p class="text-center error-300">${signUpError}</p>`: ''
           }
-          <button class="button button--primary">Login</button>
+          <button class="button button--primary">SignUp</button>
         </form>
-        <a href="#" class="block text-center js-signup-link">Create account</a>
+        <a href="#" class="block text-center js-login-link">login</a>
       </section>
-  `;
+  `
 }
 
 function listenSubmitForm() {
-  const form =  document.querySelector(".js-login-form")
+  const form =  document.querySelector(".js-signup-form")
 
   form.addEventListener("submit", async (event) => {
     try {
       event.preventDefault();
-  
+
       const { email, password } = event.target;
-  
-      const credentials = {
+      await signup({
         email: email.value,
         password: password.value,
-      }
-  
-      await login(credentials)
+      })
 
-      await STORE.fetchTasks()
       DOMHandler.load(HomePage)
     } catch (error) {
-      LoginPage.state.loginError = error.message
+      signUpPage.state.signUpError = error.message
       DOMHandler.reload()
     }
   })
 }
 
-function listenSignUpPage() {
-  const form =  document.querySelector(".js-signup-link")
+function listenLogInPage() {
+  const form =  document.querySelector(".js-login-link")
 
   form.addEventListener("click",  (event) => {
     event.preventDefault()
-
-    DOMHandler.load(signUpPage)
+    DOMHandler.load(LoginPage)
   })
 }
 
-const LoginPage = {
+const signUpPage = {
   toString() {
     return render()
   },
   addListeners() {
     listenSubmitForm()
-    listenSignUpPage()
+    listenLogInPage()
   },
   state: {
-    loginError: null,
+    signUpError: null,
   }
 }
 
-export default LoginPage;
+export default signUpPage;
